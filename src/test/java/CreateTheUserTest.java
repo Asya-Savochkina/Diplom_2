287702580f7077fe9f800a9d0c78129b98d7867b
Asya Savochkina <p0morka@yandex.ru>
2022-11-13 01:02:47 +0300

    add tests for user

--- src/test/java/CreateTheUserTest.java
+++ src/test/java/CreateTheUserTest.java
@@ -11,6 +11,10 @@ public class CreateTheUserTest {
     UserClient userClient;
     CreateTheUserRequest createTheUserRequest;
 
+    private static final String EXPECTED_MESSAGE_DUBLE = "User already exists";
+    private static final String EXPECTED_MESSAGE_EMPTY_REQUIRED_FIELD = "Email, password and name are required fields";
+
+
     @Before
     public void setup() {
         createTheUserRequest = CreateTheUserRequest.getUserAllRequiredField();
@@ -26,5 +30,27 @@ public class CreateTheUserTest {
                 .and()
                 .assertThat().body("success", equalTo(true));
     }
+
+    @Test
+    @DisplayName("Создание уже зарегистрированного пользователя")
+    @Description("Проверяем сценарий создания дубля пользователя. Ожидаем, что возвращается код 403 и \"message\": \"User already exists ")
+    public void createExistingUser() {
+        userClient.getUniqUser(createTheUserRequest.getUserAllRequiredField());
+        userClient.getUniqUser(createTheUserRequest.getUserAllRequiredField())
+                .then().statusCode(403)
+                .and()
+                .assertThat().body("message", equalTo(EXPECTED_MESSAGE_DUBLE));
+    }
+
+    @Test
+    @DisplayName("Создание пользователя с незаполненным обязательным полем")
+    @Description("Проверяем сценарий создания дубля пользователя. Ожидаем, что возвращается код 403 и \"message\": \"Email, password and name are required fields ")
+    public void createUserWithoutAllRequiredField() {
+        userClient.getUniqUser(createTheUserRequest.getUserWithoutMail())
+                .then().statusCode(403)
+                .and()
+                .assertThat().body("message", equalTo(EXPECTED_MESSAGE_EMPTY_REQUIRED_FIELD));
+    }
+
 
 }
